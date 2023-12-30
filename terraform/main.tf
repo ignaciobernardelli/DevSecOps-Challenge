f77b51662974a6e8936c6efd8a0e61f688806b00
Ignacio Bernardelli <ignacio.bernardelli@gmail.com>
2023-12-30 01:22:09 -0300

    Add terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,17 +13,27 @@ terraform {
   }
 }
 
+resource "google_service_account" "cloud-functions-sa" {
+  account_id   = "cloud-functions-sa"
+  display_name = "Service Account para Cloud Functions"
+  project      = var.project_id
+}
+
 # Variables y outputs (variables.tf y outputs.tf) - Define tus variables y outputs aquí
 
 # Recursos específicos
-module "pubsub" {
-  source = "./pubsub"  # Estructura de módulo para crear recursos de Cloud Pub/Sub
+/* module "pubsub" {
+  source = "./modules/pubsub"  # Estructura de módulo para crear recursos de Cloud Pub/Sub
 }
 
 module "functions" {
-  source = "./functions"  # Estructura de módulo para crear funciones de Cloud Functions
+  source = "./modules/functions"  # Estructura de módulo para crear funciones de Cloud Functions
 }
 
 module "bigquery" {
-  source = "./bigquery"  # Estructura de módulo para crear recursos de BigQuery
+  source = "./modules/bigquery"  # Estructura de módulo para crear recursos de BigQuery
 }
+
+module "cloudrun" {
+  source = "./modules/cloudrun"  # Estructura de módulo para crear recursos de CloudRun
+} */
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,11 @@
 variable "project_id" {
   description = "ID del proyecto GCP"
+  type = string
+  default = "devsecops-challenge"
 }
 
 variable "region" {
   description = "Región de GCP"
+  type = string
+  default = "us-east1" 
 }
